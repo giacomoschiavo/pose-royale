cb58338e548300c6ac1271d31bf435293c2da8b8
Giacomo Schiavo <42863424+giacomoschiavo@users.noreply.github.com>
2023-10-16 01:07:38 +0200

    working prototype of the canvas

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
 import "./App.css";
-import WebcamPoseDetector from "./Canvas";
+import Canvas from "./Canvas";
 
 function App() {
   return (
     <div className="App">
-      <head>
-        <script
-          src="https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision/vision_bundle.js"
-          crossorigin="anonymous"
-        ></script>
-      </head>
-      <WebcamPoseDetector />
+      <Canvas />
     </div>
   );
 }
--- src/Canvas.js
+++ src/Canvas.js
@@ -4,124 +4,89 @@ import {
   FilesetResolver,
   DrawingUtils,
 } from "@mediapipe/tasks-vision";
+import Webcam from "react-webcam";
 
-const WebcamPoseDetector = () => {
-  const videoRef = useRef(null);
+const Canvas = () => {
+  const webcamRef = useRef(null);
   const canvasRef = useRef(null);
-  const enableWebcamButtonRef = useRef(null);
   const [poseLandmarker, setPoseLandmarker] = useState(null);
-  const [webcamRunning, setWebcamRunning] = useState(false);
-  const videoHeight = "360px";
-  const videoWidth = "480px";
-  const runningMode = "IMAGE";
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    const createPoseLandmarker = async () => {
-      const vision = await FilesetResolver.forVisionTasks(
-        "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision/wasm"
-      );
+    // draw video on canvas
+    const canvasElement = canvasRef.current;
+    const canvasCtx = canvasRef.current.getContext("2d");
+    const drawingUtils = new DrawingUtils(canvasCtx);
 
-      const poseLandmarkerInstance = await PoseLandmarker.createFromOptions(
-        vision,
-        {
-          baseOptions: {
-            modelAssetPath:
-              "https://storage.googleapis.com/mediapipe-models/pose_landmarker/pose_landmarker_lite/float16/1/pose_landmarker_lite.task",
-            delegate: "GPU",
-          },
-          runningMode: runningMode,
-          numPoses: 2,
+    const draw = () => {
+      if (webcamRef.current === null) return;
+      const video = webcamRef.current.video;
+      canvasElement.width = video.videoWidth;
+      canvasElement.height = video.videoHeight;
+
+      let lastVideoTime = -1;
+      let startTimeMs = performance.now();
+      if (poseLandmarker && !loading) {
+        if (video.currentTime !== lastVideoTime) {
+          poseLandmarker.detectForVideo(video, startTimeMs, (result) => {
+            canvasCtx.save();
+            canvasCtx.clearRect(
+              0,
+              0,
+              canvasElement.width,
+              canvasElement.height
+            );
+            for (const landmark of result.landmarks) {
+              drawingUtils.drawLandmarks(landmark, {
+                radius: (data) =>
+                  DrawingUtils.lerp(data.from.z, -0.15, 0.1, 5, 1),
+              });
+              drawingUtils.drawConnectors(
+                landmark,
+                PoseLandmarker.POSE_CONNECTIONS
+              );
+            }
+            canvasCtx.restore();
+          });
         }
-      );
-
-      setPoseLandmarker(poseLandmarkerInstance);
+      }
+      requestAnimationFrame(draw);
     };
 
-    createPoseLandmarker();
+    draw();
+  }, [poseLandmarker, loading]);
+
+  useEffect(() => {
+    async function initPoseLandmarker() {
+      const vision = await FilesetResolver.forVisionTasks(
+        // path/to/wasm/root
+        "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@latest/wasm"
+      );
+      const poseLandmarker = await PoseLandmarker.createFromOptions(vision, {
+        baseOptions: {
+          modelAssetPath:
+            "https://storage.googleapis.com/mediapipe-models/pose_landmarker/pose_landmarker_lite/float16/1/pose_landmarker_lite.task",
+          delegate: "GPU",
+        },
+        runningMode: "VIDEO",
+      });
+
+      setPoseLandmarker(poseLandmarker);
+      console.log("loaded");
+    }
+
+    initPoseLandmarker();
   }, []);
 
-  const enableCam = async () => {
-    if (!poseLandmarker) {
-      console.log("Wait! poseLandmaker not loaded yet.");
-      return;
-    }
-
-    if (webcamRunning) {
-      setWebcamRunning(false);
-      enableWebcamButtonRef.current.innerText = "ENABLE PREDICTIONS";
-    } else {
-      setWebcamRunning(true);
-      enableWebcamButtonRef.current.innerText = "DISABLE PREDICTIONS";
-    }
-
-    const constraints = {
-      video: true,
-    };
-
-    try {
-      const stream = await navigator.mediaDevices.getUserMedia(constraints);
-      videoRef.current.srcObject = stream;
-      videoRef.current.addEventListener("loadeddata", predictWebcam);
-    } catch (error) {
-      console.error("Error accessing webcam:", error);
-    }
-  };
-
-  let lastVideoTime = -1;
-  const predictWebcam = async () => {
-    canvasRef.current.style.height = videoHeight;
-    videoRef.current.style.height = videoHeight;
-    canvasRef.current.style.width = videoWidth;
-    videoRef.current.style.width = videoWidth;
-
-    if (runningMode === "IMAGE") {
-      await poseLandmarker.setOptions({ runningMode: "VIDEO" });
-    }
-
-    let startTimeMs = performance.now();
-
-    if (lastVideoTime !== videoRef.current.currentTime) {
-      lastVideoTime = videoRef.current.currentTime;
-
-      poseLandmarker.detectForVideo(videoRef.current, startTimeMs, (result) => {
-        const canvasCtx = canvasRef.current.getContext("2d");
-        canvasCtx.save();
-        canvasCtx.clearRect(
-          0,
-          0,
-          canvasRef.current.width,
-          canvasRef.current.height
-        );
-
-        const drawingUtils = new DrawingUtils(canvasCtx);
-        for (const landmark of result.landmarks) {
-          drawingUtils.drawLandmarks(landmark, {
-            radius: (data) => DrawingUtils.lerp(data.from.z, -0.15, 0.1, 5, 1),
-          });
-          drawingUtils.drawConnectors(
-            landmark,
-            PoseLandmarker.POSE_CONNECTIONS
-          );
-        }
-
-        canvasCtx.restore();
-      });
-    }
-
-    if (webcamRunning) {
-      window.requestAnimationFrame(predictWebcam);
-    }
-  };
-
   return (
-    <div>
-      <button onClick={enableCam} ref={enableWebcamButtonRef}>
-        ENABLE PREDICTIONS
-      </button>
-      <video ref={videoRef} id="webcam" autoPlay />
-      <canvas ref={canvasRef} id="output_canvas" />
-    </div>
+    <>
+      <div>
+        <Webcam ref={webcamRef} />
+        <canvas ref={canvasRef} />
+      </div>
+      <button onClick={() => setLoading(false)}>Load Skeleton</button>
+    </>
   );
 };
 
-export default WebcamPoseDetector;
+export default Canvas;
