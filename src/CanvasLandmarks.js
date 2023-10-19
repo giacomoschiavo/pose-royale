a2f9e9f9679dc8f376321ef884e0f259726c7245
Giacomo Schiavo <42863424+giacomoschiavo@users.noreply.github.com>
2023-10-19 11:40:37 +0200

    summary
    code refactir

--- src/CanvasLandmarks.js
+++ src/CanvasLandmarks.js
@@ -1,11 +1,12 @@
 import React, { useEffect, useRef, useState } from "react";
-import {
-  PoseLandmarker,
-  FilesetResolver,
-  DrawingUtils,
-} from "@mediapipe/tasks-vision";
 import Webcam from "react-webcam";
-import { buildSquares } from "./detection";
+import {
+  buildSquares,
+  drawGuidelines,
+  filterLandmarks,
+  initPoseLandmarker,
+  drawSquares,
+} from "./detection";
 import tpose from "./poses/tpose.json";
 
 const CanvasLandmarks = ({
@@ -21,8 +22,6 @@ const CanvasLandmarks = ({
     // draw video on canvas
     const canvasElement = canvasRef.current;
     const canvasCtx = canvasRef.current.getContext("2d");
-    const drawingUtils = new DrawingUtils(canvasCtx);
-    const idPoses = [0, 11, 12, 13, 14, 19, 20, 23, 24, 25, 26, 27, 28];
 
     const draw = () => {
       if (webcamRef.current === null) return;
@@ -43,78 +42,25 @@ const CanvasLandmarks = ({
               canvasElement.height
             );
 
-            // draw a line in the middle of the canvas
-            canvasCtx.beginPath();
-            canvasCtx.moveTo(canvasElement.width / 2, 0);
-            canvasCtx.lineTo(canvasElement.width / 2, canvasElement.height);
-            canvasCtx.strokeStyle = "red";
-            canvasCtx.stroke();
-            canvasCtx.closePath();
-            canvasCtx.beginPath();
-            canvasCtx.moveTo(0, canvasElement.height / 2);
-            canvasCtx.lineTo(canvasElement.width, canvasElement.height / 2);
-            canvasCtx.stroke();
-            canvasCtx.closePath();
-            canvasCtx.beginPath();
-            canvasCtx.moveTo(0, canvasElement.height * 0.9);
-            canvasCtx.lineTo(canvasElement.width, canvasElement.height * 0.9);
-            canvasCtx.strokeStyle = "red";
-            canvasCtx.stroke();
-            canvasCtx.closePath();
+            drawGuidelines(canvasElement, canvasCtx);
 
-            let newLandmarks = {};
-            for (const landmark of result.landmarks) {
-              // create new filtered landmarks
-              for (let i = 0; i < landmark.length; i++) {
-                if (idPoses.includes(i)) {
-                  newLandmarks[i] = {
-                    x: landmark[i].x,
-                    y: landmark[i].y,
-                  };
-                }
-              }
-              // drawingUtils.drawConnectors(
-              //   landmark,
-              //   PoseLandmarker.POSE_CONNECTIONS
-              // );
-              // drawingUtils.drawLandmarks(newLandmarks, {
-              //   color: "red",
-              //   fillColor: "red",
-              //   radius: 3,
-              // });
+            // pick only chosen ids
+            const idPoses = [0, 11, 12, 13, 14, 19, 20, 23, 24, 25, 26, 27, 28];
+            let newLandmarks = filterLandmarks(result.landmarks, idPoses);
 
-              const squares = buildSquares(newLandmarks);
-              // console.log(squares);
-              for (let i = 0; i < squares.length; i++) {
-                let square = squares[i];
-                canvasCtx.beginPath();
-                canvasCtx.lineWidth = "1";
-                canvasCtx.rect(
-                  square.x * canvasElement.width,
-                  square.y * canvasElement.height,
-                  square.side * canvasElement.width,
-                  square.side * canvasElement.height
-                );
-                canvasCtx.strokeStyle = "red";
-                canvasCtx.stroke();
-              }
-            }
-
-            // draw rect of tpose
             const squareSide = 0.08;
-            for (const [key, value] of Object.entries(tpose)) {
-              canvasCtx.beginPath();
-              canvasCtx.lineWidth = "1";
+            const squares = buildSquares(newLandmarks, squareSide);
+            // draw squares of the skeleton
+            drawSquares(canvasElement, canvasCtx, squares, squareSide, "red");
 
-              canvasCtx.rect(
-                value.x * canvasElement.width,
-                value.y * canvasElement.height,
-                squareSide * canvasElement.width,
-                squareSide * canvasElement.height
-              );
-              canvasCtx.strokeStyle = "blue";
-              canvasCtx.stroke();
-            }
+            // draw squares of the pose (tpose)
+            drawSquares(
+              canvasElement,
+              canvasCtx,
+              Object.values(tpose),
+              squareSide,
+              "blue"
+            );
 
             canvasCtx.restore();
             setLandmarks(newLandmarks);
@@ -128,24 +74,10 @@ const CanvasLandmarks = ({
   }, [poseLandmarker, loading, setLandmarks]);
 
   useEffect(() => {
-    async function initPoseLandmarker() {
-      const vision = await FilesetResolver.forVisionTasks(
-        // path/to/wasm/root
-        "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@latest/wasm"
-      );
-      const poseLandmarker = await PoseLandmarker.createFromOptions(vision, {
-        baseOptions: {
-          modelAssetPath:
-            "https://storage.googleapis.com/mediapipe-models/pose_landmarker/pose_landmarker_lite/float16/1/pose_landmarker_lite.task",
-          delegate: "GPU",
-        },
-        runningMode: "VIDEO",
-      });
+    initPoseLandmarker((poseLandmarker) => {
       setPoseLandmarker(poseLandmarker);
-    }
-
-    initPoseLandmarker();
-  }, []);
+    });
+  }, [setPoseLandmarker]);
 
   return (
     <div className="container">
